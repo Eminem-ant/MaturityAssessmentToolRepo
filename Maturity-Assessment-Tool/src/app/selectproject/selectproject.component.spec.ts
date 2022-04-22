657a33f97293a3232b678d8202b87ffea49b15fc
Eminem-ant <54942455+Eminem-ant@users.noreply.github.com>
2022-04-22 17:59:10 +0530

    Second Commit

--- Maturity-Assessment-Tool/src/app/selectproject/selectproject.component.spec.ts
+++ Maturity-Assessment-Tool/src/app/selectproject/selectproject.component.spec.ts
@@ -2,24 +2,3 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectprojectComponent } from './selectproject.component';
 
-describe('SelectprojectComponent', () => {
-  let component: SelectprojectComponent;
-  let fixture: ComponentFixture<SelectprojectComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SelectprojectComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SelectprojectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
